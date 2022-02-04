aefc1fe28aea12f50cac423a8a41d16fb541b52b
Chris <Chris-James@users.noreply.github.com>
2022-02-03 19:08:27 -0800

    Update fileUtilities.js

--- src/fileUtilities.js
+++ src/fileUtilities.js
@@ -1,22 +1,17 @@
-const fs = require('fs')
+import * as fs from 'fs';
 
-const readFile = (path, opts = 'utf8') =>
+export const readFile = (path, opts = 'utf8') =>
   new Promise((res, rej) => {
     fs.readFile(path, opts, (err, data) => {
       if (err) rej(err)
       else res(data)
     })
-  })
+  });
 
-const writeFile = (path, data, opts = 'utf8') =>
+export const writeFile = (path, data, opts = 'utf8') =>
   new Promise((res, rej) => {
     fs.writeFile(path, data, opts, err => {
       if (err) rej(err)
       else res()
     })
-  })
-
-module.exports = {
-  readFile,
-  writeFile
-}
+  });
